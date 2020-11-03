f8af97ed72e9afe3873c4b19baea2a5e20352abe
Benedict Ahunu-Kumi <47798179+BenedictAK@users.noreply.github.com>
2020-11-02 21:34:17 -0500

    Edited Player one level And deleted extra plarycontroller scrpt

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask whatIsGround;
-   
+
     private bool grounded;
 
     //private bool doubleJumped;
@@ -34,24 +34,34 @@ public class PlayerController : MonoBehaviour
 
 
         //'GetKeyDown' input will only call once when clicked 
-        if (Input.GetKeyDown(KeyCode.Space) && grounded) 
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             //JUMP
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
         }
-      
+
         //'GetKey' Will continue calling the input as long as the key is pressed
         if (Input.GetKey(KeyCode.A))
         {
-            //LEFT
+            //"to the LEFT to the left"
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
-            
         }
-        if (Input.GetKey(KeyCode.D))
+        else
         {
-            //RIGHT
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
-        }
+            if (Input.GetKey(KeyCode.A) && !grounded)
+            {
+                //"to the LEFT to the left"
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, jumpHeight -= 0);
+            }
 
+
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                //RIGHT
+                GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+            }
+
+        }
     }
 }
