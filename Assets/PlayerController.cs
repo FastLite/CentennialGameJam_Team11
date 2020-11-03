1e8f842de89f5467e152dc9f0311d188e77665b0
Benedict Ahunu-Kumi <47798179+BenedictAK@users.noreply.github.com>
2020-11-02 19:33:23 -0500

    PlayerMovementScript

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpHeight;
+
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+   
+    private bool grounded;
+
+    //private bool doubleJumped;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //'GetKeyDown' input will only call once when clicked 
+        if (Input.GetKeyDown(KeyCode.Space) && grounded) 
+        {
+            //JUMP
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+        }
+      
+        //'GetKey' Will continue calling the input as long as the key is pressed
+        if (Input.GetKey(KeyCode.A))
+        {
+            //LEFT
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
+            
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //RIGHT
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+        }
+
+    }
+}
