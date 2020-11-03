569d247e1d378df125475ac04c32c08bd05ed7c0
Benedict Ahunu-Kumi <47798179+BenedictAK@users.noreply.github.com>
2020-11-03 10:57:02 -0500

    Disable PlayController script and Made mcController on the player

---
+++ Assets/Scripts/mcController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mcController : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    public float moveInput;
+
+    private int extraJumps;
+    public int extraJumpValue;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        extraJumps = extraJumpValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        moveInput = Input.GetAxis("Horizontal");
+        Debug.Log(moveInput);
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        
+        if(facingRight == false && moveInput > 0)
+        {
+            Flip();
+            Debug.Log("I flipped left");
+        }else if(facingRight == true && moveInput < 0)
+        {
+            Flip();
+            Debug.Log("I flipped right");
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGrounded == true)
+        {
+            extraJumps = extraJumpValue;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+
+        }else if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded){
+            rb.velocity = Vector2.up * jumpForce;
+
+        }
+    }
+}
