30cb2cc8e7a76f593a9139a59df5345d74342a5e
Benedict Ahunu-Kumi <47798179+BenedictAK@users.noreply.github.com>
2020-11-04 01:49:16 -0500

    Created Ghost Controls and Disabled the Player on Ghost Player Spawn

---
+++ Assets/Scripts/GhostSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostSpawner : MonoBehaviour
+{
+    public Transform ghostSpawner;
+    public GameObject GhostPrefab;
+
+    private bool GhostExits;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Calls the 'GoingGhost' function and confirms the ghost exited the body 
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        { 
+            GoingGhost();
+            GhostExits = true; 
+        }
+        if (GhostExits == true) { Destroy(gameObject); }
+
+       
+
+
+        void GoingGhost()
+        {  
+            // Instantiates the 'GhostPrefab' object if it didn't Exit yet then prevents it from instantiating if it exists
+            if (GhostExits == false)
+            {
+                Instantiate(GhostPrefab, ghostSpawner.position, ghostSpawner.rotation);
+                GhostExits = true;
+            }
+            else if (GhostExits == true)
+            {
+                return;
+            } 
+        }
+
+
+        
+    }
+
+    
+
+}
